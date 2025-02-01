49e52507de167d8789c60c3d1220c056dad0214b
singhrajpiyush@hmail.com <singhrajpiyush@gmail.com>
2025-02-01 13:02:02 +0530

    The Last Life

---
+++ CollectionFramework_V1/src/main/java/net/kanth/datetime_algo/CalculateAge_Pract1.java
@@ -0,0 +1,19 @@
+package net.kanth.datetime_algo;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+public class CalculateAge_Pract1 {
+	
+	
+	public static void main(String[] args) {
+		
+		LocalDate localDate = LocalDate.now();
+		LocalDate dobDate = LocalDate.parse("1999-10-08");
+		
+		Period period = Period.between(dobDate, localDate);
+		System.out.println("Your Age is\n"+period.getYears()+" years, "+period.getMonths()+ " months "+period.getDays()+" days");
+		
+	}
+
+}
---
+++ NK_Preparation/src/net/kanth/ArrayListThreadSafe.java
@@ -0,0 +1,49 @@
+package net.kanth;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.ArrayList;
+public class ArrayListThreadSafe {
+
+//Exception in thread "main" java.util.ConcurrentModificationException
+//ArrayList is not a Thread-Safe
+//ArrayList cannot perform read and write operation at the same time. we cannot modified list while reading
+		
+/**
+To Overcome this you have to use CopyOnWriteArrayList
+While Iterating loop it is getting from original CopyOnWriteArrayList
+But while adding java internally create copy of original CopyOnWriteArrayList
+At the end of the loop the reference copy of original list is dump into the original CopyOnWriteArrayList  	
+**/
+	public static void main(String[] args) {
+		
+		List<Integer> lst = new ArrayList<Integer>();
+		List<Integer> lst2 = new CopyOnWriteArrayList<>();
+	
+		lst.add(1);
+		lst.add(2);
+		lst.add(3);
+		lst.add(5);
+		lst.add(6);
+		
+		try {
+		for(int i : lst) {
+			if(i == 5) {
+				lst.add(6);
+			}
+		}
+		}catch(Exception ex) {ex.printStackTrace(); }
+		
+		lst2.add(1);
+		lst2.add(2);
+		lst2.add(4);
+		
+		for(int i : lst2) {
+			if( i == 2) {
+				lst2.add(3);
+			}
+		}
+		
+		System.out.println(lst2);
+	}
+	
+}
